765be5089d2225247e46f2fc5b71ed62e822de7b
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2017-03-08 18:42:43 -0300

    Output em arquivo no formato CSV.

--- 01 - Modelos de dados e XML/codigo.py
+++ 01 - Modelos de dados e XML/codigo.py
@@ -2,10 +2,14 @@
 
 from xml.dom.minidom import parse
 import xml.dom.minidom
-#import os
+import os
 
-#os.mkdir("dadosMarvel")
-#file = open("herois.csv", 'w')
+if os.path.exists("dadosMarvel") == False:
+    os.mkdir("dadosMarvel")
+
+if os.path.exists("dadosMarvel/herois.csv"):
+    os.remove("dadosMarvel/herois.csv")
+file = open("dadosMarvel/herois.csv", "w")
 
 dom = parse("marvel_simplificado.xml")
 universe = dom.documentElement
@@ -13,12 +17,12 @@ heroAttributes = ['name', 'popularity', 'alignment', 'gender', 'height_m', 'weig
 
 heroes = universe.getElementsByTagName("hero")
 for hero in heroes:
-    print(hero.getAttribute("id"), end = "")
+    #print(hero.getAttribute("id"), end = "")
+    file.write(hero.getAttribute("id"))
     for attr in heroAttributes:
-        print(", %s" % hero.getElementsByTagName(attr)[0].childNodes[0].data, end = "")
-    print("")
+        #print(", %s" % hero.getElementsByTagName(attr)[0].childNodes[0].data, end = "")
+        file.write(", %s" % hero.getElementsByTagName(attr)[0].childNodes[0].data)
+    #print("")
+    file.write("\n")
 
-
-
-
-
+file.close()
