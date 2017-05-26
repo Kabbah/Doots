cd85cf822879733192ad3c289cd7eb720f6dfddf
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2017-05-25 23:05:10 -0300

    Hello there!

--- 10 - Etapa Final - Aplicação/dbConn.php
+++ 10 - Etapa Final - Aplicação/dbConn.php
@@ -1,12 +1,14 @@
 <?php
-    define("HOST", "localhost");     // Para o host com o qual você quer se conectar.
-    define("DBUSER", "root");    // O nome de usuário para o banco de dados. 
-    define("DBPASS", "");    // A senha do banco de dados. 
-    define("DBNAME", "Doots");    // O nome do banco de dados.
 
-    $conn = new mysqli(HOST, DBUSER, DBPASS, DBNAME);
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());     
-        exit();
-    }
+define("HOST", "localhost");     // Para o host com o qual você quer se conectar.
+define("DBUSER", "root");    // O nome de usuário para o banco de dados. 
+define("DBPASS", "");    // A senha do banco de dados. 
+define("DBNAME", "Doots");    // O nome do banco de dados.
+
+$conn = new mysqli(HOST, DBUSER, DBPASS, DBNAME);
+// Verifica a conexão.
+if($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
 ?>
