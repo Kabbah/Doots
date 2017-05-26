b329425012921cd379b821bff7b49f635f08a5fc
Angelin01 <angeloborsoiross@hotmail.com>
2017-05-25 22:56:07 -0300

    Trocado variáveis por defines

--- 10 - Etapa Final - Aplicação/dbConn.php
+++ 10 - Etapa Final - Aplicação/dbConn.php
@@ -1,10 +1,10 @@
 <?php
-    $host = "localhost";
-    $dbUser = "root";
-    $dbPass = "";
-    $dbName = "Doots";
+    define("HOST", "localhost");     // Para o host com o qual você quer se conectar.
+    define("DBUSER", "root");    // O nome de usuário para o banco de dados. 
+    define("DBPASS", "");    // A senha do banco de dados. 
+    define("DBNAME", "Doots");    // O nome do banco de dados.
 
-    $conn = new mysqli($host, $dbUser, $dbPass, $dbName);
+    $conn = new mysqli(HOST, DBUSER, DBPASS, DBNAME);
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());     
         exit();
