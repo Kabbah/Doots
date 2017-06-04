d1581a0cf903d29bebbdfc61f2c2e9a81fa939ef
Angelin01 <angeloborsoiross@hotmail.com>
2017-06-03 22:29:48 -0300

    Feito banner para o topo das páginas. Usar require nele

---
+++ 10 - Etapa Final - Aplicação/banner.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/w3.css">
+
+
+<header class="w3-container w3-purple">
+    <h1 class="w3-left"><a class="logo" href="#">Doots</a></h1> <!-- Vai virar uma imagem -->
+    <?php
+        if(isset($_SESSION['login'])) {
+            echo '<div class="w3-right">
+                    <h2 class="welcome">Bem vindo '.$_SESSION['login'].'</h2>
+                    <img class="avatar welcome" src="avatares/avatar.png" class="w3-round" alt="Avatar">
+                    <a href="settings.php"><img class="welcome" id="cog" src="imagens/cog.png" alt="cog"></a>
+                </div>';
+        }
+        else {
+            echo '<div class="w3-right">
+                    <h2 class="w3-btn"><a href="registerLogin.php">Logar ou Registrar</a></h2>
+                  </div>';
+        }
+    ?>
+</header>
