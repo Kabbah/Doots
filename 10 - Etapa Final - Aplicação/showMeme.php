e34d88ae63fe13ac55b43750b9121b902a9d6b20
Angelin01 <angeloborsoiross@hotmail.com>
2017-06-09 22:11:54 -0300

    Começado comentarios, ainda EXTREMAMENTE BUGADO. Criar mais de 1 comentário para testar

--- 10 - Etapa Final - Aplicação/showMeme.php
+++ 10 - Etapa Final - Aplicação/showMeme.php
@@ -19,7 +19,13 @@ $stmt->close();
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
-        <title>Postar um Meme - Doots</title>
+        <title><?php if($arquivo != NULL && $deletado == false)  { 
+                            echo $titulo;
+                        } 
+                        else { 
+                            echo "Meme não encontrado";
+                        } 
+            ?> - Doots</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/w3.css">
@@ -34,32 +40,40 @@ $stmt->close();
             <?php
                 
                 if($arquivo != NULL && $deletado == false) {
-                    // Isso aqui vai estar feio para caramba, é só um protótipo ainda
                     
-                    echo '<h2 class="meme-title">' . $titulo . '</h2>' .
-                         '<div class="meme-image"><img src="memes/' . $arquivo . '"></div>';
+                    // Tem que colocar botão pra up/down doot do lado do título
+                    echo '<p class="meme-title">' . $titulo . '</p>' .
+                         '<span class="w3-small meme-time">Postado as ' . date_format(date_create($datahora), "H:i d/m/Y") . ' por ' . $loginUsuario . '</span>' .
+                         '<div class="meme-image"><img src="memes/' . $arquivo . '"></div>' .
+                         '<div></div>'; // Reservado pra input de comentário
                     
                     if($countComentarios > 0) {
-                        $stmt = $conn->prepare("SELECT Comentario.conteudo, Comentario.doots, Comentario.dataHora, Comentario.editado, Comentario.deletado, Comentario.dataHoraEdit, Usuario.login FROM Comentario INNER JOIN Usuario ON Comentario.idUsuario = Usuario.id WHERE Comentario.idMeme = ?");
+                        $stmt = $conn->prepare("SELECT Comentario.conteudo, Comentario.doots, Comentario.dataHora, Comentario.editado, Comentario.deletado, Comentario.dataHoraEdit, Usuario.login, Usuario.avatar FROM Comentario INNER JOIN Usuario ON Comentario.idUsuario = Usuario.id WHERE Comentario.idMeme = ?");
                         $stmt->bind_param("s", $_GET["meme"]);
                         $stmt->execute();
                         
-                        $stmt->bind_result($textoComentario, $dootsComentario, $datahoraComentario, $editadoComentario, $deletadoComentario, $datahoraeditComentario, $loginUsuarioComentario);
+                        $stmt->bind_result($textoComentario, $dootsComentario, $datahoraComentario, $editadoComentario, $deletadoComentario, $datahoraeditComentario, $loginUsuarioComentario, $avatarUsuarioComentario);
                         while($stmt->fetch()) {
-                            echo "<div>" .
-                                    "<p>$textoComentario</p>" .
-                                    "<p>$loginUsuarioComentario</p>" .
-                                "</div>";
+                            echo '<div class="comment-wrapper">
+                                    <div class="comment-author w3-left">
+                                        <img class="avatar" src="avatares/' . $avatarUsuarioComentario . '">
+                                        <p>' . $loginUsuarioComentario . '</p>
+                                        <p class="w3-tiny">' . date_format(date_create($datahoraComentario), "H:i d/m/Y") . '
+                                    </div>
+                                    <p>' . $textoComentario . '</p>
+                                 </div>';
+
+                                    
                         }
                         $stmt->close();
                     }
                     else {
-                        echo "<p>Não há comentários.</p>";
+                        echo '<h4 class="comment-wrapper">Ainda não há comentários. Escreva um você! <b>AGORA!</b></h4>';
                     }
                 }
                 else {
-                    echo "<h1>Meme não encontrado</h1>";
-                    echo "<p>Este meme não existe, ou foi deletado.</p>";
+                    echo '<h1 class="meme-title">Hic Sunt Dracones</h1>';
+                    echo '<h2 class="w3-container">Este meme não existe ou foi deletado</h2>';
                 }
                 
                 $conn->close();
