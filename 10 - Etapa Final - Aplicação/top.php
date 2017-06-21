3fa51e577ef1d55be4a4bf38819a0206ca4c3532
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2017-06-21 14:32:09 -0300

    Barra de navegação nas front pages + Font Awesome.

--- 10 - Etapa Final - Aplicação/top.php
+++ 10 - Etapa Final - Aplicação/top.php
@@ -13,12 +13,24 @@ if(!isset($_SESSION["login"])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/w3.css">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
     </head>
     
     <body>
         <?php
             require('banner.php');
         ?>
+        <div class="w3-bar w3-purple">
+            <a href="/" class="w3-bar-item w3-button w3-mobile">populares</a>
+            <a href="new.php" class="w3-bar-item w3-button w3-mobile">novos</a>
+            <a href="top.php" class="w3-bar-item w3-button w3-mobile" style="background-color:#812092;">no topo</a>
+            <form class="w3-bar-item w3-right w3-mobile" style="padding: 0px;">
+                <input type="text" class="w3-bar-item w3-input" style="background-color:#eac0f1;" placeholder="Buscar...">
+                <button type="submit" class="w3-bar-item w3-button w3-right">
+                    <i class="fa fa-search"></i>
+                </button>
+            </form>
+        </div>
         <div>
             <!-- Isso vai ser feio pra caramba, é só pra ficar meio navegável. -->
             <p><a href="createMeme.php">Criar um meme</a></p>
