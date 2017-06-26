c5fcafea3b3ace88de71d364ec757a6f62e7c2c0
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2017-06-26 12:21:49 -0300

    Correção de timezone na edição de comentário + arquivos HTACCESS.

--- 10 - Etapa Final - Aplicação/edit_comment.php
+++ 10 - Etapa Final - Aplicação/edit_comment.php
@@ -28,6 +28,8 @@ if($stmt->num_rows == 0 || $idUsuario != $_SESSION["id"]) {
 }
 $stmt->close();
 
+date_default_timezone_set("America/Sao_Paulo");
+
 $stmt = $conn->prepare("UPDATE Comentario SET conteudo = ?, editado = 1, dataHoraEdit = ? WHERE id = ?");
 $stmt->bind_param("sss", $_POST["commentText"], date("Y-m-d H:i:s"), $_POST["commentID"]);
 $stmt->execute();
