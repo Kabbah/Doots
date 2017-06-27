f53a59205ae6947d664001b042b94ce9279c7627
GusTmmer <gustimmer@gmail.com>
2017-06-26 23:08:29 -0300

    Mudanca no layout dos settings

--- 10 - Etapa Final - Aplicação/settings.php
+++ 10 - Etapa Final - Aplicação/settings.php
@@ -43,48 +43,54 @@ require ("dbConn.php");
                     echo '<div class="error"><p class="w3-panel w3-red"><b>' . $_COOKIE['avatarVazio'] . '</b></p></div>';
                 }
             ?>
-        
-            <h3 class="w3-purple form-title">Mudar Avatar</h3>
-            <form class="w3-container w3-border-bottom w3-border-left w3-border-right w3-border-top w3-animate-opacity log-reg" method="post" enctype="multipart/form-data" action="processAvatar.php">
-                <br/>
-                <label class="w3-text-purple">Avatar</label>
-                <input class="w3-input w3-border" type="file" name="avatar" required>
-
-                <br/>
-                <input class="w3-button w3-purple form-submit-button" type="submit">
-            </form>
-
-            <h3 class="w3-purple form-title">Mudar Email</h3>
-            <form class="w3-container w3-border-bottom w3-border-left w3-border-right w3-animate-opacity log-reg" method="post" action="processEmail.php">
-                <br/>
-                <label class="w3-text-purple">Email</label>
-                <input class="w3-input w3-border" type="text" name="email" required>
-
-                <br/>
-                <label class="w3-text-purple">Confirmar Email</label>
-                <input class="w3-input w3-border" type="text" name="email-confirm" required>
-
-                <br/>
-                <input class="w3-button w3-purple form-submit-button" type="submit">
-            </form>
             
-            <h3 class="w3-purple form-title">Mudar Senha</h3>
-            <form class="w3-container w3-border-bottom w3-border-left w3-border-right w3-animate-opacity log-reg" method="post" action="processPassword.php">
-                <br/>
-                <label class="w3-text-purple">Senha Atual</label>
-                <input class="w3-input w3-border" type="password" name="password-now" required>
+            <div class="settings-tab">
+                <h3 class="w3-purple form-title">Mudar Avatar</h3>
+                <form class="w3-container w3-border-bottom w3-border-left w3-border-right w3-border-top w3-animate-opacity log-reg" method="post" enctype="multipart/form-data" action="processAvatar.php">
+                    <br/>
+                    <label class="w3-text-purple">Avatar</label>
+                    <input class="w3-input w3-border" type="file" name="avatar" required>
 
-                <br/>
-                <label class="w3-text-purple">Nova Senha</label>
-                <input class="w3-input w3-border" type="password" name="password" required>
+                    <br/>
+                    <input class="w3-button w3-purple form-submit-button" type="submit">
+                </form>
+            </div>
+            
+            <div class="settings-tab">
+                <h3 class="w3-purple form-title">Mudar Email</h3>
+                <form class="w3-container w3-border-bottom w3-border-left w3-border-right w3-animate-opacity log-reg" method="post" action="processEmail.php">
+                    <br/>
+                    <label class="w3-text-purple">Email</label>
+                    <input class="w3-input w3-border" type="text" name="email" required>
 
-                <br/>
-                <label class="w3-text-purple">Confirmar Senha</label>
-                <input class="w3-input w3-border" type="password" name="password-confirm" required>
+                    <br/>
+                    <label class="w3-text-purple">Confirmar Email</label>
+                    <input class="w3-input w3-border" type="text" name="email-confirm" required>
 
-                <br/>
-                <input class="w3-button w3-purple form-submit-button" type="submit">
-            </form>
+                    <br/>
+                    <input class="w3-button w3-purple form-submit-button" type="submit">
+                </form>
+            </div>
+            
+            <div class="settings-tab">
+                <h3 class="w3-purple form-title">Mudar Senha</h3>
+                <form class="w3-container w3-border-bottom w3-border-left w3-border-right w3-animate-opacity log-reg" method="post" action="processPassword.php">
+                    <br/>
+                    <label class="w3-text-purple">Senha Atual</label>
+                    <input class="w3-input w3-border" type="password" name="password-now" required>
+
+                    <br/>
+                    <label class="w3-text-purple">Nova Senha</label>
+                    <input class="w3-input w3-border" type="password" name="password" required>
+
+                    <br/>
+                    <label class="w3-text-purple">Confirmar Senha</label>
+                    <input class="w3-input w3-border" type="password" name="password-confirm" required>
+
+                    <br/>
+                    <input class="w3-button w3-purple form-submit-button" type="submit">
+                </form>
+            </div>
         </div>
     </body>
 </html>
